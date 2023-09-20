1658695782e6e13b3f960ca6accd254f3dccde74
Roman Sereda <sereda@ozzylogik.com>
2023-09-20 12:06:01 +0300

    add control

--- gui/controls.cpp
+++ gui/controls.cpp
@@ -1,1 +1,33 @@
 #include "controls.hpp"
+#include "imgui_internal.h"
+
+namespace Ui {
+    std::atomic_int Control::mIdCounter = 0;
+
+	Control::Control(const std::string& text)
+        : mText(text), mId(mIdCounter++) {
+    }
+
+    Control::Control(const std::string& text, std::unique_ptr<std::function<void()>>& clicked) : Control(text) {
+        mOnClicked = std::move(clicked);
+    }
+
+	void Control::display() {
+        ImGui::PushStyleColor(ImGuiCol_::ImGuiCol_Border, mStyle.border);
+        ImGui::PushStyleColor(ImGuiCol_Button, mStyle.color);
+        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, mStyle.hovered);
+        ImGui::PushStyleColor(ImGuiCol_ButtonActive, mStyle.active);
+
+        ImGui::PushStyleVar(ImGuiStyleVar_::ImGuiStyleVar_FrameBorderSize, 1.0f);
+        ImGui::PushStyleVar(ImGuiStyleVar_::ImGuiStyleVar_FrameRounding, 1.0f);
+        ImGui::PushStyleVar(ImGuiStyleVar_::ImGuiStyleVar_FramePadding, mFramePadding);
+
+        ImGui::PushID(mId);
+        bool clicked = ImGui::Button(mText.c_str());
+        ImGui::PopID();
+
+        ImGui::PopStyleColor(4);
+        ImGui::PopStyleVar(3);
+	}
+
+}
--- gui/controls.hpp
+++ gui/controls.hpp
@@ -1,5 +1,37 @@
 #pragma once
+#include <vector>
+#include <string>
+#include <functional>
+#include <memory>
+#include <atomic>
+
+#include "imgui.h"
 
 namespace Ui {
+	struct ControlStyle {
+		ImVec4 color = ImColor(0.2f, 0.2f, 0.2f, 1.0f);
+		ImVec4 border = ImColor(0.5f, 0.5f, 0.5f, 1.0f);
+		ImVec4 hovered = ImColor(0.3f, 0.3f, 0.3f, 1.0f);
+		ImVec4 active = ImColor(1.0f, 0.6f, 0.0f, 1.0f);
+	};
+
+	class Control {
+	public:
+		Control(const std::string& text);
+		Control(const std::string& text, std::unique_ptr<std::function<void()>>& clicked);
+
+		void display();
+
+	private:
+		int mId = -1;
+		static std::atomic_int mIdCounter;
+
+		std::string mText;
+		std::unique_ptr<std::function<void()>> mOnClicked {nullptr};
+
+		ControlStyle mStyle;
+
+		ImVec2 mFramePadding = { 3.0f, 1.0f };
+	};
 
 }
